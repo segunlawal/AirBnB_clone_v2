bd4d1e2176c677e77bed0ebe30e8988d878005f0
Segun <lawalsegun72@gmail.com>
2023-06-21 12:05:59 -0700

    update class attribute

--- models/base_model.py
+++ models/base_model.py
@@ -2,24 +2,23 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
 
 
 Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            self.id = Column(String(60), nullable=False, primary_key=True)
-            self.created_at = Column(
-                                DateTime,
-                                nullable=False,
-                                default=datetime.datetime.utcnow)
-            self.updated_at = Column(
-                                DateTime,
-                                nullable=False,
-                                default=datetime.datetime.utcnow)
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
