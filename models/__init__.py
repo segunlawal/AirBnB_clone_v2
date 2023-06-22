d98d8880dbf53c3cc0dc369371ccda1aa4dc3e79
Segun <lawalsegun72@gmail.com>
2023-06-22 15:57:07 -0700

    create instance conditionally

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+from os import getenv
 
 
-storage = FileStorage()
-storage.reload()
+db_env = getenv("HBNB_TYPE_STORAGE")
+
+if db_env == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
