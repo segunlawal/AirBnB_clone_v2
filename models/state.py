4c82734a820e1999505d85f2c7d90574d937c16b
Segun <lawalsegun72@gmail.com>
2023-06-22 14:59:19 -0700

    add getter to State class

--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    name = Column(String(128), nullable=False)
+    __tablename__ = 'states'
+    cities = relationship("City", backref="state", cascade="all, delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """getter attribute that returns the list of City instances
+            """
+            from models import storage
+            from models.city import City
+            list_city = [city for city in storage.all(City).values()
+                         if city.state_id == self.id]
+            return list_city
