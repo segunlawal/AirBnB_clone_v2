05bf4f0b65c981542bea5154a0a90b33968d12c6
Segun <lawalsegun72@gmail.com>
2023-07-06 02:05:11 -0700

    deploy archive

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This module contains the do_deploy function"""
+import os
+from fabric.api import run, env, cd, put
+from datetime import datetime
+env.hosts = ['52.91.128.8', '100.24.240.21']
+env.user = "ubuntu"
+
+
+def do_pack():
+    """Fabric script that distributes to web servers"""
+    if not os.path.exists("archive_path"):
+        return False
+
+    else:
+        try:
+            filename = '/data/web_static/releases/{}'.format(
+                    archive_path.strip(".tgz"))
+            archive = archive_path.lstrip('versions/')
+            run('mkdir -p {}'.format(filename))
+            put('{}'.format(archive_path), "/tmp/")
+            run('tar -xzf /tmp/{} -C {}'.format(archive, filename))
+            run('mv {}/web_static/* {}'.format(filename, filename))
+            run('rm -rf {}/web_static/'.format(filename))
+            run('rm /tmp/{}'.format(archive))
+            run('rm /data/web_static/current')
+            run('ln -sf {} /data/web_static/current'.format(filename))
+            return True
+        except Exception:
+            return False
