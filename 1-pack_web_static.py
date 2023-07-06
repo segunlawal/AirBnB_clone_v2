0a99654c4012e4c8ca52aba63473861d11a2027e
Segun <lawalsegun72@gmail.com>
2023-07-06 01:38:53 -0700

    create versions folder with p flag

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """This module contains the do_pack function"""
-import os
 from fabric.api import local
 from datetime import datetime
 
@@ -8,8 +7,7 @@ from datetime import datetime
 def do_pack():
     """Fabric script that generates a .tgz archive from the
     contents of the web_static folder"""
-    if not os.path.exists("versions"):
-        local("mkdir versions")
+    local("mkdir -p versions")
 
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     archive = "versions/web_static_{}.tgz".format(timestamp)
