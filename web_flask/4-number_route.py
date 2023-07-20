9912eab11eb02d85863b9ee39e5a5941ca170698
Segun <lawalsegun72@gmail.com>
2023-07-20 16:20:14 -0700

    script starts Flask web app

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Returns a string"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns a string"""
+    return "HBNB!"
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """Returns a string"""
+    return "C {}".format(text.replace("_", " "))
+
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text='cool'):
+    """Returns a string"""
+    return "Python {}".format(text.replace("_", " "))
+
+@app.route('/number/<n>', strict_slashes=False)
+def display_python(n):
+    """Returns a string"""
+    if isinstance(n, int):
+        return "{} is a number".format(n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
