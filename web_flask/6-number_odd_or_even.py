0d489c89ac0c88e21b1b23fa29456ce481ca3f28
Segun <lawalsegun72@gmail.com>
2023-07-21 13:12:50 -0700

    add dynamic route

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -30,7 +30,7 @@ def display_python(text='cool'):
     return "Python {}".format(text.replace("_", " "))
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def display_number(n):
     """Returns a string"""
     if isinstance(n, int):
