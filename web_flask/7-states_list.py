e7738e68b725189987abe0d75639a9403b237527
Segun <lawalsegun72@gmail.com>
2023-07-21 20:15:22 -0700

    script adds routes

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application"""
+
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def display_states():
+    """Displays HTML page"""
+    states = storage.all()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def close_database(self):
+    """Remove current Session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
